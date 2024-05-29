5f8b3a5355583a73ccbc9f3d96f7535d8834eb37
Shine-5705 <guptashine5002@gmail.com>
2024-05-30 02:04:00 +0530

    added Setup and reuirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT ='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+setup(
+name='FULL_END_PROJECT',
+version = '0.01',
+author = 'Shine-5705',
+author_email='guptashine5002@gmail.com',
+packages = find_packages(),
+install_requierments = get_requirements('requirements.txt')
+
+)
